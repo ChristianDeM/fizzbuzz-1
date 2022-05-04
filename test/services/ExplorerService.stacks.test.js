d86bffba753375f43e5c915c41c1cd3fdeea8a25
ChristianDeM <Christian.d.martino@gmail.com>
2022-05-04 14:42:14 -0500

    Ejecutando linter

--- test/services/ExplorerService.stacks.test.js
+++ test/services/ExplorerService.stacks.test.js
@@ -3,7 +3,7 @@ const  explorers = require("./explorers.test.json");
 
 describe("Tests para ExplorerService Filter Stack", () => {
     test("Requerimiento 1: Calcular todos los explorers Cons stack", () => {       
-        const stack="javascript"
+        const stack="javascript";
         const explorersInNode = ExplorerService.getFiltertBystack(explorers,"javascript");
         expect(explorersInNode[0].stacks[0]).toBe("javascript");
     });
